62756f675a87fd454c6d86926990a2f78bb4a87d
Jon <unseldjonas0@gmail.com>
2024-04-12 19:05:13 +0200

    Update items.lua

--- items/items.lua
+++ items/items.lua
@@ -1,12 +1,155 @@
-watering_can                 = {name = "watering_can", 				    label = 'watering can', 			weight = 500, 		type = 'item', 		image = "watering_can.png", 		    unique = false, 	useable = false, 	shouldClose = false,    combinable = nil,   description = 'simple watering can'},
-weed_lemonhaze_seed		    = {name = 'weed_lemonhaze_seed', 			label = 'weed lemonhaze seed', 	    weight = 20, 		type = 'item', 		image = 'weed_lemonhaze_seed.png', 		unique = false, 	useable = true, 	shouldClose = true,	    combinable = nil,   description = 'Seeeds'},
-weed_lemonhaze				 = {name = 'weed_lemonhaze', 			  	label = 'weed lemonhaze', 			weight = 20, 		type = 'item', 		image = 'weed_lemonhaze.png', 		   	unique = false, 	useable = false, 	shouldClose = false,	combinable = nil,   description = 'Funny Description'},
-coca_seed 				     = {name = 'coca_seed', 			  	   	label = 'coca seed', 			    weight = 20, 		type = 'item', 		image = 'coca_seed.png', 		   	    unique = false, 	useable = true, 	shouldClose = true,	    combinable = nil,   description = 'Funny Description'},
-coca 				        = {name = 'coca', 			  	   	label = 'coca', 			    weight = 20, 		type = 'item', 		image = 'coca.png', 		   	    unique = false, 	useable = true, 	shouldClose = true,	    combinable = nil,   description = 'Funny Description'},
-paper 				 		 = {name = 'paper', 			    		label = 'paper', 					weight = 50, 	    type = 'item', 		image = 'paper.png', 				    unique = false, 	useable = false, 	shouldClose = false,    combinable = nil,   description = 'Funny Description'},
-nitrous 				     = {name = 'nitrous', 			    		label = 'nitrous', 					weight = 500, 	    type = 'item', 		image = 'nitrous.png', 				    unique = false, 	useable = false, 	shouldClose = false,    combinable = nil,   description = 'Funny Description'},
-fertilizer 				     = {name = 'fertilizer', 			    		label = 'fertilizer', 					weight = 500, 	    type = 'item', 		image = 'nitrous.png', 				    unique = false, 	useable = false, 	shouldClose = false,    combinable = nil,   description = 'Funny Description'},
-cocaine 				     = {name = 'cocaine', 			    		label = 'cocaine', 					weight = 20, 	    type = 'item', 		image = 'fertilizer.png', 				    unique = false, 	useable = false, 	shouldClose = false,     combinable = nil,   description = 'Funny Description'},
-joint 				 		 = {name = 'joint', 			    		label = 'joint', 					weight = 10, 	    type = 'item', 		image = 'joint.png', 				    unique = false, 	useable = true, 	shouldClose = true,     combinable = nil,   description = 'Funny Description'},
-weed_processing_table 		 = {name = 'weed_processing_table', 	    label = 'weed processing table', 	weight = 50, 	    type = 'item', 		image = 'weed_processing_table.png', 	unique = false, 	useable = true, 	shouldClose = true,     combinable = nil,   description = 'Funny Description'},
-cocaine_processing_table 	 = {name = 'cocaine_processing_table', 	    label = 'cocaine processing table', weight = 50, 	    type = 'item', 		image = 'cocaine_processing_table.png', unique = false, 	useable = true, 	shouldClose = true,     combinable = nil,   description = 'Funny Description'},
+watering_can = {
+    name = "watering_can",
+    label = 'Watering can',
+    weight = 500,
+    type = 'item',
+    image = "watering_can.png",
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'simple watering can'
+},
+
+weed_lemonhaze_seed = {
+    name = 'weed_lemonhaze_seed',
+    label = 'Weed Lemonhaze Seed',
+    weight = 20,
+    type = 'item', 		
+    image = 'weed_lemonhaze_seed.png', 		
+    unique = false, 	
+    useable = true, 	
+    shouldClose = true,	    
+    combinable = nil,   
+    description = 'Weed Lemonhaze Seed'
+},
+
+weed_lemonhaze = {
+    name = 'weed_lemonhaze',
+    label = 'weed lemonhaze',
+    weight = 20,
+    type = 'item',
+    image = 'weed_lemonhaze.png',
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'Weed Lemonhaze'
+},
+
+coca_seed = {
+    name = 'coca_seed',
+    label = 'coca seed',
+    weight = 20,
+    type = 'item',
+    image = 'coca_seed.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Coca Seed'
+},
+
+coca = {
+    name = 'coca',
+    label = 'coca',
+    weight = 20,
+    type = 'item',
+    image = 'coca.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Coca'
+},
+
+paper = {
+    name = 'paper',
+    label = 'paper',
+    weight = 50,
+    type = 'item',
+    image = 'paper.png',
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'Paper'
+},
+
+nitrous = {
+    name = 'nitrous',
+    label = 'nitrous',
+    weight = 500,
+    type = 'item',
+    image = 'nitrous.png',
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'nitrous'
+    },
+
+fertilizer = {
+    name = 'fertilizer',
+    label = 'fertilizer',
+    weight = 500,
+    type = 'item',
+    image = 'nitrous.png',
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'fertilizer'
+},
+
+cocaine = {
+    name = 'cocaine',
+    label = 'cocaine',
+    weight = 20,
+    type = 'item',
+    image = 'fertilizer.png',
+    unique = false,
+    useable = false,
+    shouldClose = false,
+    combinable = nil,
+    description = 'cocaine'
+    },
+
+joint = {
+    name = 'joint',
+    label = 'joint',
+    weight = 10,
+    type = 'item',
+    image = 'joint.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'joint'
+},
+
+weed_processing_table = {
+    name = 'weed_processing_table',
+    label = 'weed processing table',
+    weight = 50,
+    type = 'item',
+    image = 'weed_processing_table.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Process some weed'
+},
+
+cocaine_processing_table = {
+    name = 'cocaine_processing_table',
+    label = 'cocaine processing table',
+    weight = 50,
+    type = 'item',
+    image = 'cocaine_processing_table.png',
+    unique = false,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'Process some cocaine'
+},
