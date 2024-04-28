73c0fda3fe5c8df5cf41225e00e1b07afca11668
Jon <unseldjonas0@gmail.com>
2024-04-29 00:04:06 +0200

    Delete cl_functions.lua

--- client/cl_functions.lua
+++
@@ -1,6 +0,0 @@
-function PoliceAlert()
-    -- Add Your alert system here
-	-- exports['ps-dispatch']:DrugSale() -- (PS DISPATCH)
-	TriggerServerEvent('police:server:policeAlert', 'Drug sale in progress') -- (DEFAULT QBCore)
-	if Config.Debug then print('Police Notify Function triggered') end
-end
