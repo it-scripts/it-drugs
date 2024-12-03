b3366c2f9aa749f289f830942320b212e071a0bc
turnpoint <realturnpoint@gmail.com>
2024-12-03 18:12:24 +0100

    item fix
    items fix

--- items/ox_items.lua
+++ items/ox_items.lua
@@ -63,6 +63,94 @@
         image = "weed_lemonhaze.png",
     }
 },
+
+["weed_og_seed"] = {
+    label = "Weed Og Seed",
+    weight = 20,
+    stack = true,
+    close = true,
+    description = "Weed Og Seed",
+    client = {
+        image = "weed_og_seed.png",
+    }
+},
+
+["weed_og"] = {
+    label = "weed Og",
+    weight = 20,
+    stack = true,
+    close = false,
+    description = "weed Og",
+    client = {
+        image = "weed_og.png",
+    }
+},
+
+["weed_purple_haze_seed"] = {
+    label = "Weed Purple Haze Seed",
+    weight = 20,
+    stack = true,
+    close = true,
+    description = "Weed Purple Haze Seed",
+    client = {
+        image = "weed_purple_haze_seed.png",
+    }
+},
+
+["weed_purple_haze"] = {
+    label = "weed Purple Haze",
+    weight = 20,
+    stack = true,
+    close = false,
+    description = "weed Purple Haze",
+    client = {
+        image = "weed_purple_haze.png",
+    }
+},
+
+["weed_white_widow_seed"] = {
+    label = "Weed White Widow Seed",
+    weight = 20,
+    stack = true,
+    close = true,
+    description = "Weed White Widow Seed",
+    client = {
+        image = "weed_white_widow_seed.png",
+    }
+},
+
+["weed_white_widow"] = {
+    label = "weed White Widow",
+    weight = 20,
+    stack = true,
+    close = false,
+    description = "weed White Widow",
+    client = {
+        image = "weed_white_widow.png",
+    }
+},
+
+["weed_blueberry_seed"] = {
+    label = "Weed Blueberry Seed",
+    weight = 20,
+    stack = true,
+    close = true,
+    description = "Weed Blueberry Seed",
+    client = {
+        image = "weed_blueberry_seed.png",
+    }
+},
+
+["weed_blueberry"] = {
+    label = "weed Blueberry",
+    weight = 20,
+    stack = true,
+    close = false,
+    description = "weed Blueberry",
+    client = {
+        image = "weed_blueberry.png",
+    }
+},
  
 ["coca_seed"] = {
     label = "Coca Seed",
