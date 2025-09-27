cc782cdd3ec9f9ad0541ee3d08374b24e0e50292
Jon <unseldjonas0@gmail.com>
2025-09-27 13:41:01 +0200

    update dealer handler

--- server/classes/class_dealer.lua
+++ server/classes/class_dealer.lua
@@ -5,3 +5,74 @@
 
     Copyright © 2025 AllRoundJonU <https://github.com/allroundjonu>
 ]]
+local Dealers = {}
+
+--- @class Dealer : OxClass
+--- @field id string
+local Dealer = lib.class('Dealer')
+
+function Dealer:constructor(id)
+    self.id = id
+    self.position = nil
+    ---@type table: List of items the dealer is buying
+    self.buyItems = {}
+    ---@type table: List of items the dealer is selling
+    self.sellItems = {}
+
+    Dealers[self.id] = self
+
+    self:generatePosition()
+end
+
+function Dealer:generatePosition()
+    local locations = Config.DrugDealers[self.id].locations
+    math.randomseed(os.time() + #locations)
+
+    self.position = locations[math.random(1, #locations)]
+    Dealers[self.id] = self -- Update the dealer in the Dealers table with the new position
+end
+
+function Dealer:generateSellItemData(item)
+    local priceData = Config.DrugDealers[self.id].items['selling'][item]
+    math.randomseed(os.time() + #self.sellItems)
+
+    self.sellItems[item] = {
+        price = math.random(priceData.min, priceData.max),
+        moneyType = priceData.moneyType,
+        --amount = math.random(priceData.amount.min, priceData.amount.max)
+    }
+    Dealers[self.id] = self -- Update the dealer in the Dealers table with the new sell item
+end
+
+function Dealer:generateBuyItemData(item)
+    local priceData = Config.DrugDealers[self.id].items['buying'][item]
+    math.randomseed(os.time() + #self.sellItems)
+
+    self.buyItems[item] = {
+        price = math.random(priceData.min, priceData.max),
+        moneyType = priceData.moneyType,
+        --amount = math.random(priceData.amount.min, priceData.amount.max)
+    }
+    Dealers[self.id] = self
+end
+
+function Dealer:getPosition()
+    return self.position
+end
+
+function Dealer:getSellItemData(item)
+    return self.sellItems[item]
+end
+
+function Dealer:getBuyItemData(item)
+    return self.buyItems[item]
+end
+
+function Dealer:getData()
+    return {
+        id = self.id,
+        position = self.position,
+        buyItems = self.buyItems,
+        sellItems = self.sellItems
+    }
+end
